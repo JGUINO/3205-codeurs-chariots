e9690cf4a31c1a5d0833e3ddd44269d0bb412dba
Jérôme GUILLON <jerome.guillon@cmc-coffrages.com>
2018-05-29 18:23:29 +0200

    sans lire les param

--- codeursMQTTIO3.py
+++ codeursMQTTIO3.py
@@ -367,6 +367,8 @@ class MyDaemon(Daemon):
 
 if __name__ == "__main__":
 	daemon = MyDaemon(settings.PID_FILE)
+	daemon.debug=False
+	daemon.restart()
 	if len(sys.argv) == 3:
 		if 'start' == sys.argv[1]:
 			daemon.debug=False
